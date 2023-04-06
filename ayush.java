ef6b0fa7c44c0174c95899de00d3ac1de8de4447
ayush <115801704+Ayush-082@users.noreply.github.com>
2023-04-06 17:02:22 +0530

    Create ayush.java

---
+++ ayush.java
@@ -0,0 +1,22 @@
+# java-basic
+class ayush
+{
+    int i;
+    void set value(int i)
+    {
+        this.i=i;
+    }
+    void show()
+    {
+        System.out.print(i);
+    }
+}
+class ay
+{
+    public static void main(String[] args)
+    {
+        ayush t1=new ayush();
+        t1.set value(10);
+        t1.show();
+    }
+}
